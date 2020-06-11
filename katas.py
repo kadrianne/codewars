7215052c3101448335442b1a41f06161bfa187e2
kadrianne <kristine.a.du@gmail.com>
2020-06-11 09:21:58 -0600

    Welcome

---
+++ katas.py
@@ -0,0 +1,21 @@
+# 2020.06.11 - Welcome!
+def greet(language):
+    return {
+        'czech': 'Vitejte',
+        'danish': 'Velkomst',
+        'dutch': 'Welkom',
+        'english': 'Welcome',
+        'estonian': 'Tere tulemast',
+        'finnish': 'Tervetuloa',
+        'flemish': 'Welgekomen',
+        'french': 'Bienvenue',
+        'german': 'Willkommen',
+        'irish': 'Failte',
+        'italian': 'Benvenuto',
+        'latvian': 'Gaidits',
+        'lithuanian': 'Laukiamas',
+        'polish': 'Witamy',
+        'spanish': 'Bienvenido',
+        'swedish': 'Valkommen',
+        'welsh': 'Croeso'
+    }.get(language, 'Welcome')
