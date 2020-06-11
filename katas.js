155fa90de43fae43821450381f6140032fb21e16
kadrianne <kristine.a.du@gmail.com>
2020-06-11 10:37:46 -0600

    Solve Persistent Bugger.

--- katas.js
+++ katas.js
@@ -1,24 +1,39 @@
-// 2020.06.11 - Welcome!
-function greet(language){
-    const messages = {
-        english: 'Welcome',
-        czech: 'Vitejte',
-        danish: 'Velkomst',
-        dutch: 'Welkom',
-        estonian: 'Tere tulemast',
-        finnish: 'Tervetuloa',
-        flemish: 'Welgekomen',
-        french: 'Bienvenue',
-        german: 'Willkommen',
-        irish: 'Failte',
-        italian: 'Benvenuto',
-        latvian: 'Gaidits',
-        lithuanian: 'Laukiamas',
-        polish: 'Witamy',
-        spanish: 'Bienvenido',
-        swedish: 'Valkommen',
-        welsh: 'Croeso'
+// 2020.06.11
+    // 8kyu - Welcome!
+    function greet(language){
+        const messages = {
+            english: 'Welcome',
+            czech: 'Vitejte',
+            danish: 'Velkomst',
+            dutch: 'Welkom',
+            estonian: 'Tere tulemast',
+            finnish: 'Tervetuloa',
+            flemish: 'Welgekomen',
+            french: 'Bienvenue',
+            german: 'Willkommen',
+            irish: 'Failte',
+            italian: 'Benvenuto',
+            latvian: 'Gaidits',
+            lithuanian: 'Laukiamas',
+            polish: 'Witamy',
+            spanish: 'Bienvenido',
+            swedish: 'Valkommen',
+            welsh: 'Croeso'
+        }
+
+        return messages[language] || messages['english']
     }
 
-    return messages[language] || messages['english']
-}
+    // 6kyu - Persistent Bugger.
+    function persistence(num) {
+        let splitNumber = `${num}`.split("")
+        let count = 0
+        
+        while (splitNumber.length > 1) {
+        let product = splitNumber.reduce((accumulator,number) => accumulator * number)
+        splitNumber = `${product}`.split("")
+        count++
+        }
+        
+        return count
+    }
