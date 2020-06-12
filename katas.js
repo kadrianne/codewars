218f0df3a3c0a4d99182da527d1e2785653398e9
kadrianne <kristine.a.du@gmail.com>
2020-06-12 10:06:55 -0600

    Solve Adjacent repeated words in a string.

--- katas.js
+++ katas.js
@@ -1,3 +1,32 @@
+// 2020.06.12
+    // 6kyu - Adjacent repeated words in a string
+
+    function countAdjacentPairs(searchString) {
+        const string = searchString.toLowerCase()
+        const words = string.split(/\s+/)
+        let count = 0
+        let i = 0
+        while (i < words.length) {
+            let firstWord = words[i]
+            let j = i + 1
+            let nextWord = words[j]
+            
+            if (firstWord === nextWord) {
+                count++
+            
+                while (firstWord === nextWord && j <= words.length) {
+                    j++
+                    nextWord = words[j]
+                }
+                
+                i = j
+            } else {
+                i++
+            }
+        }
+        return count
+    }
+
 // 2020.06.11
     // 8kyu - Welcome!
     function greet(language){
