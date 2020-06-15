196b10a68c1a659fc5245a51b89cb6d16f02e8a3
kadrianne <kristine.a.du@gmail.com>
2020-06-15 09:30:10 -0600

    Solve How many days...

--- katas.py
+++ katas.py
@@ -1,3 +1,20 @@
+# 2020.06.15 - How many days are we represented in a foreign country?
+
+def days_represented(trips):
+    days_represented = {}
+    count = 0
+    for day in range(1,366):
+        days_represented[day] = 0
+
+    for start,end in trips:
+        for date in range(start,end + 1):
+            days_represented[date] = True
+
+    for value in days_represented.values():
+        if value == True:
+            count += 1
+    return count
+
 # 2020.06.11 - Welcome!
 def greet(language):
     return {
