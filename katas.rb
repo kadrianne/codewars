2e09de48a5621fc54b84fc2120092c36a7b39c4b
Kristine Du <kristine.a.du@gmail.com>
2020-09-01 10:44:20 -0600

    Solve Title Case.

--- katas.rb
+++ katas.rb
@@ -1,3 +1,26 @@
+# 2020.09.01
+    # 6kyu - Title Case
+    def title_case(title, minor_words = '')
+        splitTitle = title.capitalize.split
+        
+        i = 1
+        while i < splitTitle.length do
+            word = splitTitle[i]
+
+            if !minor_words.downcase.split.include?(word.downcase)
+                splitTitle[i] = word.capitalize
+            end
+            
+            i += 1
+        end
+        
+        splitTitle.join(" ")
+    end
+
+    def title_case(title, minor_words = '')
+        title.capitalize.split.map {|word| minor_words.downcase.split.include?(word) ? word : word.capitalize}.join(" ")
+    end
+
 # 2020.06.18
     # 7kyu - Alternate capitalization
     def capitalize(s)
